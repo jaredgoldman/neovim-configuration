267f59c18a715d484f50c031133d4410a9d59a0a
jaredgoldman <jaredgoldmandev@gmail.com>
2024-04-28 15:10:40 -0400

    feat: add ts/js react to formatter

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,30 +1,32 @@
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		javascript = { { "prettierd", "prettier" } },
-		typescript = { { "prettierd", "prettier" } },
-		json = { { "prettierd", "prettier" } },
-		markdown = { { "marksman", "prettierd", "prettier" } },
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    javascript = { { "prettierd", "prettier" } },
+    typescript = { { "prettierd", "prettier" } },
+    javascriptreact = { "prettierd", "prettier" },
+    typescriptreact = { "prettierd", "prettier" },
+    json = { { "prettierd", "prettier" } },
+    markdown = { { "marksman", "prettier", "prettierd" } },
+  },
 })
 
 local M = {}
 
 M.format = function(args)
-	local range = nil
-	if args and args.count ~= -1 then
-		-- Assuming args.line1 and args.line2 are provided and valid
-		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-		range = {
-			start = { args.line1, 0 },
-			["end"] = { args.line2, end_line:len() },
-		}
-	end
-	require("conform").format({ async = true, lsp_fallback = true, range = range })
+  local range = nil
+  if args and args.count ~= -1 then
+    -- Assuming args.line1 and args.line2 are provided and valid
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format({ async = true, lsp_fallback = true, range = range })
 end
 
 vim.api.nvim_create_user_command("Format", function(args)
-	M.format(args)
+  M.format(args)
 end, { range = true })
 
 return M
--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -4,45 +4,45 @@ harpoon.setup({})
 -- basic telescope configuration
 local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
 
-	require("telescope.pickers")
-		.new({}, {
-			prompt_title = "Harpoon",
-			finder = require("telescope.finders").new_table({
-				results = file_paths,
-			}),
-			previewer = conf.file_previewer({}),
-			sorter = conf.generic_sorter({}),
-		})
-		:find()
+  require("telescope.pickers")
+      .new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      })
+      :find()
 end
 
 local function toggle_mark()
-	local current_file = api.nvim_buf_get_name(0) -- Get the current buffer's file path
-	local item, index = harpoon:list():get_by_value(current_file)
-	if item then
-		harpoon:list():remove_at(index)
-		print("Removed from Harpoon: " .. current_file)
-	else
-		harpoon:list():add({ value = current_file })
-		print("Added to Harpoon: " .. current_file)
-	end
+  local current_file = api.nvim_buf_get_name(0) -- Get the current buffer's file path
+  local item, index = harpoon:list():get_by_value(current_file)
+  if item then
+    harpoon:list():remove_at(index)
+    print("Removed from Harpoon: " .. current_file)
+  else
+    harpoon:list():add({ value = current_file })
+    print("Added to Harpoon: " .. current_file)
+  end
 end
 
 -- Commands
 api.nvim_create_user_command("HarpoonClear", function()
-	harpoon:list():clear()
+  harpoon:list():clear()
 end, { bang = true })
 api.nvim_create_user_command("HarpoonAdd", function()
-	harpoon:list():add()
+  harpoon:list():add()
 end, { bang = true })
 api.nvim_create_user_command("HarpoonToggleFile", function()
-	toggle_mark()
+  toggle_mark()
 end, { bang = true })
 api.nvim_create_user_command("HarpoonUI", function()
-	toggle_telescope(harpoon:list())
+  toggle_telescope(harpoon:list())
 end, { bang = true })
